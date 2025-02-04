a7e194ed8dc72df597c7009085dade655f073351
kdh5790 <k_0106@naver.com>
2025-02-04 17:56:02 +0900

    reset

--- obj/Debug/net8.0/TxtRPG.AssemblyInfo.cs
+++ obj/Debug/net8.0/TxtRPG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TxtRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f2aa9a48e7dbcf6b0141dd958cd54c807f9a3a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15ab090b613a7bf0b03de7ce2ade09a97a7d37ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("TxtRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TxtRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
